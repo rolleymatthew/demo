603243b50c73efd4e2ea58df733d6ac4c8697718
yunwang <yunwang@sohu-inc.com>
2021-05-20 15:16:06 +0800

    自定义配置文件，快速读取到bean

--- 01primary/src/main/java/com/wy/bean/Student.java
+++ 01primary/src/main/java/com/wy/bean/Student.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+
 /**
  * Created by yunwang on 2021/5/20 14:33
  */
