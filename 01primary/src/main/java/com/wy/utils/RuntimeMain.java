49f45f73daf82db497034c8c2cf4b901c87ca738
yunwang <yunwang@sohu-inc.com>
2021-06-30 18:16:58 +0800

    计算CPU的数量

---
+++ 01primary/src/main/java/com/wy/utils/RuntimeMain.java
@@ -0,0 +1,10 @@
+package com.wy.utils;
+
+/**
+ * Created by yunwang on 2021/6/30 18:13
+ */
+public class RuntimeMain {
+    public static void main(String[] args) {
+        System.out.println(""+Runtime.getRuntime().availableProcessors());
+    }
+}
