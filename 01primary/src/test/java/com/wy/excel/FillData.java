899f65369bcb4844352e85334ed415042a0426e4
yunwang <yunwang@sohu-inc.com>
2022-03-23 16:27:28 +0800

    excel用模板填充数据

---
+++ 01primary/src/test/java/com/wy/excel/FillData.java
@@ -0,0 +1,17 @@
+package com.wy.excel;
+
+import java.util.Date;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class FillData {
+    private String name;
+    private double number;
+    private Date date;
+
+}
